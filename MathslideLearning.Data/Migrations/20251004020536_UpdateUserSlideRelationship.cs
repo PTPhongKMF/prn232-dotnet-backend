19e65d3e12c6ccce113338df07702a3bcabb8bed
DangQuangTien <dangquangtien16102002@gmail.com>
2025-10-04 09:30:49 +0700

    Change delete behavior to Cascade for key relationships
    Updated the delete behavior for Teacher-Slide, Teacher-Exam, and User-Receipt relationships from Restrict to Cascade in the DbContext. Added IsDeleted property to the User entity. Included new EF Core migrations to reflect these changes in the database schema.

---
+++ MathslideLearning.Data/Migrations/20251004020536_UpdateUserSlideRelationship.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MathslideLearning.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserSlideRelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Slides_Users_TeacherId",
+                table: "Slides");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Users",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Slides_Users_TeacherId",
+                table: "Slides",
+                column: "TeacherId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Slides_Users_TeacherId",
+                table: "Slides");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Users");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Slides_Users_TeacherId",
+                table: "Slides",
+                column: "TeacherId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
